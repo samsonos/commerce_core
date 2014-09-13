0e71495456e27645de0e5cbe2cbf7c6e8d127c62
Vitaly Iegorov <egorov@samsonos.com>
2014-09-13 18:58:50 +0300

    Added orm classes

--- src/Core.php
+++ src/Core.php
@@ -18,7 +18,30 @@ class Core extends ExternalModule
     /** Module connection handler */
     public function prepare()
     {
+        // Create order table SQL
+        $orders = 'CREATE TABLE IF NOT EXISTS `order` (
+          `order_id` int(11) NOT NULL AUTO_INCREMENT,
+          `client_id` int(11) NOT NULL,
+          `sum` float NOT NULL,
+          `status` int(11) NOT NULL,
+          `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (`order_id`),
+          KEY `client_id` (`client_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;';
 
+        // Create payments table SQL
+        $payment = 'CREATE TABLE IF NOT EXISTS `payment` (
+          `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+          `order_id` int(11) NOT NULL,
+          `client_id` int(11) NOT NULL,
+          `sum` float NOT NULL,
+          `status` tinyint(2) NOT NULL,
+          `response` varchar(512) NOT NULL,
+          `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (`payment_id`),
+          KEY `client_id` (`client_id`),
+          KEY `order_id` (`order_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;';
 
         return parent::prepare();
     }
---
+++ src/Order.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 13.09.14 at 18:56
+ */
+ namespace samsonos\commerce;
+
+/**
+ * Generic Orders SamsonPHP commerce system class
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ * @copyright 2014 SamsonOS
+ */
+class Order extends \samson\activerecord\order
+{
+
+}
+ 
