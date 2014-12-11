3a47bc0c7e61135c31199f94f8a53d8a45b8128f
Storchovyy <storchovyy@samsonos.com>
2014-12-11 17:59:12 +0200

    extends comrpessableService

--- src/Core.php
+++ src/Core.php
@@ -4,14 +4,16 @@
  * on 13.09.14 at 18:35
  */
  namespace samsonos\commerce;
- use samson\core\ExternalModule;
+
+ use samson\core\CompressableService;
+ use samson\core\Config;
 
  /**
  * Generics SamsonPHP commerce sytem core
  * @author Vitaly Egorov <egorov@samsonos.com>
  * @copyright 2014 SamsonOS
  */
-class Core extends ExternalModule
+class Core extends CompressableService
 {
     public $id = 'commerce_core';
 
